f587dc71edfc51c863745967f813f885ea0f7717
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-04-26 10:42:12 -0500

    Refactored function to arrow

--- modules/sections.js
+++ modules/sections.js
@@ -1,13 +1,13 @@
-export function changeSection() {
-  if (this.innerHTML === "List") {
+export const changeSection = (e) => {
+    if (e.target.innerHTML === "List") {
     document.getElementById("list").style.display = "block";
     document.getElementById("add-new-book").style.display = "none";
     document.getElementById("contact").style.display = "none";
-  } else if (this.innerHTML === "Add book") {
+  } else if (e.target.innerHTML === "Add book") {
     document.getElementById("list").style.display = "none";
     document.getElementById("add-new-book").style.display = "block";
     document.getElementById("contact").style.display = "none";
-  } else if (this.innerHTML === "Contact") {
+  } else if (e.target.innerHTML === "Contact") {
     document.getElementById("list").style.display = "none";
     document.getElementById("add-new-book").style.display = "none";
     document.getElementById("contact").style.display = "block";
